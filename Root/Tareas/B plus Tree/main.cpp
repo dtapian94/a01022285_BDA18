cff3e2cae6b81f7ba4fa81859431bb4751ffec2d
Daniel Tapia <dtapian94@gmail.com>
2018-02-25 14:36:06 -0600

    Add files via upload

---
+++ Root/Tareas/B plus Tree/main.cpp
@@ -0,0 +1,27 @@
+
+//
+
+#include <iostream>
+
+#include "b_plus_tree.hpp"
+void printTree(int leave, int data){
+    std::cout << "leave" << leave;
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    mds::b_plus_tree<int, int> * arbol;
+    
+    arbol = new mds::b_plus_tree<int, int>(3,3,std::less<int>());
+    
+    arbol->insert(3, 32452);
+    arbol->insert(6, 5634);
+    arbol->insert(2, 5476);
+    arbol->insert(8, 6789);
+    arbol->insert(9, 3452);
+    arbol->insert(4, 73686);
+    std::less<int> *fun = new std::less<int>();
+    arbol->walk(*fun);
+    
+    return 0;
